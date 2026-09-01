82a4802974a3db43316e0edaad384762988ec258
Enes Çakmak <cakmakkeness@gmail.com>
2026-09-01 15:11:14 +0300

    live on prod

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,6 @@ const nextConfig = {
     unoptimized: true,
     remotePatterns: [{ protocol: "https", hostname: "image.tmdb.org" }],
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
 }
 
 export default nextConfig
